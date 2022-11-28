b80c8787cc977dcf93d797be33831b2dd86df567
ofurtumi <thorvaldur.tumi@gmail.com>
2022-11-28 18:02:52 +0000

    breyting á test.sh og kláruð 2021 queries

--- queries/test.sh
+++ queries/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # athugar hvort .sql skrain se nyrri en gagnagrunnurinn og recompilar ef svo er
-[ ../profSchema.sql -nt ./profSchema.db ] && $(echo '.save profSchema.db' | sqlite3 -init ../profSchema.sql)
+[ ../profSchema.sql -nt ./profSchema.db ] && $(echo '.save profSchema.db' | sqlite3 -init ../profSchema.sql) && echo 'Recompiled'
 
 sqlite3 profSchema.db '.read tumi-2021.sql'
